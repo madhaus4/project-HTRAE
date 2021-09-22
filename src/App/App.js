219e90a6d8b0c1db1c8a087b0eacff52f304e404
Maria DelSignore <maria.ad27@gmail.com>
2021-09-22 15:06:24 -0600

    Clean out react basic jsx

--- src/App/App.js
+++ src/App/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+     <h1>HEY PARTY PPL</h1>
     </div>
   );
 }
