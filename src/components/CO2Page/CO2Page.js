ca97a45195e8568d2e854151ef240de4fb767aa1
Maria DelSignore <maria.ad27@gmail.com>
2021-09-22 17:49:18 -0600

    Import CO2 fetched data into state

--- src/components/CO2Page/CO2Page.js
+++ src/components/CO2Page/CO2Page.js
@@ -1,6 +1,28 @@
+import { useState, useEffect } from 'react';
+import {getData} from '../../utils/apiCalls';
 import './CO2Page.css';
 
-const CO2Page = () => {}
+const CO2Page = () => {
+  const [CO2Data, setCO2Data] = useState([])
+  const [errorMsg, setErrorMsg] = useState('')
+
+  const retrieveData = async () => {
+    try {
+      let data = await getData('')
+      setCO2Data(data.co2)
+    } catch(error) {
+      setErrorMsg('ERROR MSG: ', errorMsg)
+    }
+  }
+
+  useEffect(() => {
+    retrieveData();
+  }, []);
+
+  return (
+    <p>CO2 page</p>
+  )
+}
 
 
 export default CO2Page;
