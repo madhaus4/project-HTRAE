6300323372628c5b5a26d14838f6e2dfa5c9cb19
Maria DelSignore <maria.ad27@gmail.com>
2021-09-22 16:10:29 -0600

    Update header text to nav text

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,13 +5,13 @@ const NavBar = () => {
   return (
     <nav>
       <div>
-        <button className='btn-in-header'>Home</button>
-        <button className='btn-in-header'>Carbon Dioxide</button>
-        <button className='btn-in-header'>Temperature</button>
-        <button className='btn-in-header'>Polar Ice Melt</button>
+        <button className='btn-in-nav'>Home</button>
+        <button className='btn-in-nav'>Carbon Dioxide</button>
+        <button className='btn-in-nav'>Temperature</button>
+        <button className='btn-in-nav'>Polar Ice Melt</button>
       </div>
       <img 
-        className='logo-in-header'
+        className='logo-in-nav'
         src={logo} 
         alt='project HTRAE logo: triangle with black text, black border, inside triangle is light blue, inside triangle is green, inside triangle is yellow, inside triangle is a deep pink' 
       />
