6d6fd0ffd227e9614e5e76c32fa9d77efd20651d
Maria DelSignore <maria.ad27@gmail.com>
2021-09-22 16:10:02 -0600

    Add class name to buttons in nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,13 +5,13 @@ const NavBar = () => {
   return (
     <nav>
       <div>
-        <button>Home</button>
-        <button>Carbon Dioxide</button>
-        <button>Temperature</button>
-        <button>Polar Ice Melt</button>
+        <button className='btn-in-header'>Home</button>
+        <button className='btn-in-header'>Carbon Dioxide</button>
+        <button className='btn-in-header'>Temperature</button>
+        <button className='btn-in-header'>Polar Ice Melt</button>
       </div>
       <img 
-        className='logo-inHeader'
+        className='logo-in-header'
         src={logo} 
         alt='project HTRAE logo: triangle with black text, black border, inside triangle is light blue, inside triangle is green, inside triangle is yellow, inside triangle is a deep pink' 
       />
