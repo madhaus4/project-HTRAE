6533577026de38f866c446811bff2b01c5636b83
Maria DelSignore <maria.ad27@gmail.com>
2021-09-22 16:51:10 -0600

    Pull in fetch data to test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,23 @@
+import { useState, useEffect } from 'react';
+
 import NavBar from '../NavBar/NavBar';
 import HomePage from '../HomePage/HomePage';
+import {getData} from '../../utils/apiCalls';
 import './App.css';
 
 function App() {
+  const [temperatureData, setTemperatureData] = useState([])
+
+  const pageLoad = () => {
+    getData()
+    .then(data => setTemperatureData(data.result))
+    .then(console.log('temperatureData: ', temperatureData))
+  }
+
+  useEffect(() => {
+    pageLoad()
+  })
+
   return (
     <div>
      <NavBar />
