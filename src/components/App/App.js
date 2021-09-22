8549e3b5c23340273b918a4b10f838917348825b
Maria DelSignore <maria.ad27@gmail.com>
2021-09-22 17:54:13 -0600

    Remove state from App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,18 @@
-import { useState, useEffect } from 'react';
-
+// import { useState, useEffect } from 'react';
 import NavBar from '../NavBar/NavBar';
 import HomePage from '../HomePage/HomePage';
-import {getData} from '../../utils/apiCalls';
+// import {getData} from '../../utils/apiCalls';
 import './App.css';
 
 const App = () => {
-  const [errorMsg, setErrorMsg] = useState('')
-  const [temperatureData, setTemperatureData] = useState([])
-
-  useEffect(() => {
-    const retrieveData = async () => {
-      try {
-        let data = await getData('temperature');
-        setTemperatureData(data.result)
-      } catch(error) {
-        setErrorMsg('this doesn\'t work')
-        console.log('errorMsg: ', errorMsg)
-      }
-    }
-    retrieveData();
-    console.log('temperatureData: ', temperatureData)
-  }, []);
 
   return (
-    <div>
+    <>
      <NavBar />
-     <HomePage />
-    </div>
+     <main>
+       <HomePage />
+     </main>
+    </>
   );
 }
 
