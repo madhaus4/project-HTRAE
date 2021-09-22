2bf281bae0d6660fc465ba95dfcae3a30458e229
Maria DelSignore <maria.ad27@gmail.com>
2021-09-22 15:59:02 -0600

    Update imports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
+import NavBar from '../NavBar/NavBar';
+import HomePage from '../HomePage/HomePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>HEY PARTY PPL</h1>
+    <div>
+     <NavBar />
+     <HomePage />
     </div>
   );
 }
