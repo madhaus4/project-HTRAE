43f6bc48543beb9ffec5433d52657a438040ba16
Maria DelSignore <maria.ad27@gmail.com>
2021-09-22 16:08:36 -0600

    Add elements to nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,20 @@
 import './NavBar.css';
+import logo from '../../logo.png';
 
 const NavBar = () => {
   return (
     <header>
-      <h1>howdy</h1>
+      <div>
+        <button>Home</button>
+        <button>Carbon Dioxide</button>
+        <button>Temperature</button>
+        <button>Polar Ice Melt</button>
+      </div>
+      <img 
+        className='logo-inHeader'
+        src={logo} 
+        alt='project HTRAE logo: triangle with black text, black border, inside triangle is light blue, inside triangle is green, inside triangle is yellow, inside triangle is a deep pink' 
+      />
     </header>
   )
 }
