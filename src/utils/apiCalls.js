c562390f4063671fa7b24b498b7ec3c582231efd
Maria DelSignore <maria.ad27@gmail.com>
2021-09-22 17:18:22 -0600

    Implement async await into fetch function

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,5 +1,16 @@
-export const getData = () => {
-  return fetch('https://global-warming.org/api/temperature-api ')
-    .then(res => res.json())
-    .then(data => console.log(data))
+// export const getData = async () => {
+//   return fetch(`https://global-warming.org/api/temperature-api`)
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+// }
+
+
+export const getData = async (category) => {
+  let response = await fetch(`https://global-warming.org/api/${category}-api`)
+
+  if (!response.ok) {
+    throw new Error('this no good')
+  }
+
+  return await response.json();
 }
