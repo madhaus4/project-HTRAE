a5d4d0d2662cc3271fa02335d8c19ecc83d9533f
Maria DelSignore <maria.ad27@gmail.com>
2021-09-22 18:02:33 -0600

    Add mor words to intro statement

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div>
       <h2>Welcome to project HTRAE</h2>
-      <p>resource on global warming effects</p>
+      <p>resource on global warming effects.  out here trying to reverse the direction our planet's environment is heading</p>
     </div>
   )
 }
