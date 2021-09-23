0538996f46248b40b3baf7374dfc6cd6ea50d9c1
Maria DelSignore <maria.ad27@gmail.com>
2021-09-22 20:04:34 -0600

    Import route and switch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import { Route, Switch } from 'react-router-dom';
 // import { useState, useEffect } from 'react';
 // import {getData} from '../../utils/apiCalls';
 import NavBar from '../NavBar/NavBar';
@@ -13,7 +14,13 @@ const App = () => {
     <>
      <NavBar />
      <main>
-       <HomePage />
+       <Switch>
+        <Route exact path to="/" render={() => <HomePage />} />
+        <Route exact path to="/CarbonDioxideLevels" render={() => <CO2Page />} />
+        <Route exact path to="/Temperatures" render={() => <TempPage />} />
+        <Route exact path to="/ArcticIceMelt" render={() => <PolarPage />} />
+        {/* <Route render={() => <NotFound />} /> */}
+       </Switch>
      </main>
     </>
   );
