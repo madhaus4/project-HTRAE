8a7deff426dce94f008572b57e23bc6d22cadb85
Maria DelSignore <maria.ad27@gmail.com>
2021-09-24 11:13:51 -0600

    Add basic graph to CO2Page

--- src/components/CO2Page/CO2Graph.js
+++ src/components/CO2Page/CO2Graph.js
@@ -1,16 +1,31 @@
-import {XYPlot, LineSeries} from 'react-vis';
-// import '../node_modules/react-vis/dist/style.css';
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+import '../../../node_modules/react-vis/dist/style.css';
 import './CO2Page.css';
 
+
+
 const CO2Graph = ({ CO2Data }) => {
+  // let mappedData = CO2Data.map(elem => {
+
   return (
-    <div className="App">
-      <XYPlot height={300} width={300}>
-        <LineSeries data={CO2Data} />
-      </XYPlot>
-    </div>
-  );
-}
+    <XYPlot
+      width={600}
+      height={300}>
+      <HorizontalGridLines />
+      <LineSeries
+        color="#B8466F"
+        data={ [
+          {x: 1, y: 8},
+          {x: 2, y: 10},
+          {x: 3, y: 15}
+        ]}
+      />
+      <XAxis title='X' />
+      <YAxis title='Y' />
+    </XYPlot>
+  )
+};
+
 
 // const CO2Graph = ({ CO2Data }) => {
 //   let mappedData = CO2Data.map(elem => {
