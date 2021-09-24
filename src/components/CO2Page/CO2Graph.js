c04ca47d54707a3d4ccd342f41d41d4d7cd346b6
Maria DelSignore <maria.ad27@gmail.com>
2021-09-24 10:06:06 -0600

    Import react vis into co2 graph

--- src/components/CO2Page/CO2Graph.js
+++ src/components/CO2Page/CO2Graph.js
@@ -1,17 +1,33 @@
+import {XYPlot, LineSeries} from 'react-vis';
+// import '../node_modules/react-vis/dist/style.css';
 import './CO2Page.css';
 
 const CO2Graph = ({ CO2Data }) => {
-  let mappedData = CO2Data.map(elem => {
-    return (
-      <article>
-        <h2>{`date: ${elem.month}-${elem.year}`}</h2>
-        <h2>{`cycle: ${elem.cycle}`}</h2>
-        <h2>{`trend: ${elem.trend}`}</h2>
-      </article>
-    )
-  })
-  return mappedData
+  return (
+    <div className="App">
+      <XYPlot height={300} width={300}>
+        <LineSeries data={CO2Data} />
+      </XYPlot>
+    </div>
+  );
 }
+
+// const CO2Graph = ({ CO2Data }) => {
+//   let mappedData = CO2Data.map(elem => {
+//     return (
+//       <article>
+//         <h2>{`date: ${elem.month}-${elem.year}`}</h2>
+//         <h2>{`cycle: ${elem.cycle}`}</h2>
+//         <h2>{`trend: ${elem.trend}`}</h2>
+//       </article>
+//     )
+//   })
+//   return mappedData
+// }
 
 
 export default CO2Graph;
+
+
+
+
