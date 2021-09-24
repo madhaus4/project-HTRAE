ac85e1c5b029ba4b2994be20ebffe917486fe2be
Maria DelSignore <maria.ad27@gmail.com>
2021-09-24 12:59:46 -0600

    Add in graph style

--- src/components/PolarPage/PolarGraph.js
+++ src/components/PolarPage/PolarGraph.js
@@ -1,26 +1,52 @@
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+// import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+import {XYPlot, XAxis, YAxis, MarkSeries, LineSeries, CircularGridLines} from 'react-vis';
 import '../../../node_modules/react-vis/dist/style.css';
 import './PolarPage.css';
 
-const PolarGraph = () => {
+const PolarGraph = ({ polarData }) => {
+
+  // const data = [
+  //   {r: 1, theta: Math.PI / 3, size: 30},
+  //   {r: 1.7, theta: (2 * Math.PI) / 3, size: 10},
+  //   {r: 2, theta: Math.PI, size: 1},
+  //   {r: 3, theta: (3 * Math.PI) / 2, size: 12},
+  //   {r: 2.5, theta: Math.PI / 4, size: 4},
+  //   {r: 0, theta: Math.PI / 4, size: 1}
+  // ];
+  
+  const margin = {
+    top: 10,
+    bottom: 10,
+    left: 10,
+    right: 10
+  };
+  
+  const WIDTH = 300;
+  const HEIGHT = 300;
+
   return (
     <XYPlot
-      width={600}
-      height={300}>
-      <HorizontalGridLines />
+      margin={margin}
+      xDomain={[-3, 3]}
+      yDomain={[-3, 3]}
+      width={WIDTH}
+      height={HEIGHT}
+    >
+      <CircularGridLines />
+      <XAxis top={(HEIGHT - margin.top) / 2} />
+      <YAxis left={(WIDTH - margin.left - margin.right) / 2} />
       <LineSeries
-        color="#B8466F"
-        data={ [
-          {x: 1, y: 8},
-          {x: 2, y: 10},
-          {x: 3, y: 15}
-        ]}
+        strokeWidth={2}
+        sizeRange={[5, 15]}
+        data={polarData.map(elem => ({
+          ...elem,
+          x: Math.cos(elem.extent) * elem.extent,
+          y: Math.sin(elem.area) * elem.area
+        }))}
       />
-      <XAxis title='X' />
-      <YAxis title='Y' />
     </XYPlot>
   )
-}
+};
 
 
 export default PolarGraph;
