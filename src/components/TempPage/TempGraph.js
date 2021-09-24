500d8f0ffe7d29b1d2b571346a05e252905915d7
Maria DelSignore <maria.ad27@gmail.com>
2021-09-24 12:23:08 -0600

    Import graph package

--- src/components/TempPage/TempGraph.js
+++ src/components/TempPage/TempGraph.js
@@ -1,10 +1,24 @@
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+import '../../../node_modules/react-vis/dist/style.css';
 import './TempPage.css';
 
 const TempGraph = () => {
   return (
-    <div>
-      <p>this is a graph</p>
-    </div>
+    <XYPlot
+      width={600}
+      height={300}>
+      <HorizontalGridLines />
+      <LineSeries
+        color="#B8466F"
+        data={ [
+          {x: 1, y: 8},
+          {x: 2, y: 10},
+          {x: 3, y: 15}
+        ]}
+      />
+      <XAxis title='X' />
+      <YAxis title='Y' />
+    </XYPlot>
   )
 }
 
