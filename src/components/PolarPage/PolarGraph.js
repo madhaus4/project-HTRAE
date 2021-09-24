8b1ee0e9ac7e37e74ec64b531f9ee7df474b62e3
Maria DelSignore <maria.ad27@gmail.com>
2021-09-24 12:22:40 -0600

    Add base graph code

--- src/components/PolarPage/PolarGraph.js
+++ src/components/PolarPage/PolarGraph.js
@@ -1,8 +1,24 @@
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+import '../../../node_modules/react-vis/dist/style.css';
 import './PolarPage.css';
 
 const PolarGraph = () => {
   return (
-    <p>this a graph</p>
+    <XYPlot
+      width={600}
+      height={300}>
+      <HorizontalGridLines />
+      <LineSeries
+        color="#B8466F"
+        data={ [
+          {x: 1, y: 8},
+          {x: 2, y: 10},
+          {x: 3, y: 15}
+        ]}
+      />
+      <XAxis title='X' />
+      <YAxis title='Y' />
+    </XYPlot>
   )
 }
 
