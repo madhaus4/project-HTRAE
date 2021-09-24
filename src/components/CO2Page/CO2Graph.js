4d33d61a1cd4b7b8becfa24924c326a24a779aaa
Maria DelSignore <maria.ad27@gmail.com>
2021-09-23 22:47:00 -0600

    Render data to page without graph

--- src/components/CO2Page/CO2Graph.js
+++ src/components/CO2Page/CO2Graph.js
@@ -1,9 +1,16 @@
 import './CO2Page.css';
 
-const CO2Graph = () => {
-  return (
-    <p>this a graph</p>
-  )
+const CO2Graph = ({ CO2Data }) => {
+  let mappedData = CO2Data.map(elem => {
+    return (
+      <article>
+        <h2>{`date: ${elem.month}-${elem.year}`}</h2>
+        <h2>{`cycle: ${elem.cycle}`}</h2>
+        <h2>{`trend: ${elem.trend}`}</h2>
+      </article>
+    )
+  })
+  return mappedData
 }
 
 
