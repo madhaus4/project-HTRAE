1f234a7ba027863303113ce28ee67e788fa52142
Maria DelSignore <maria.ad27@gmail.com>
2021-09-25 16:37:50 -0600

    Generate a random tip

--- src/components/TipsPage/TipsPage.js
+++ src/components/TipsPage/TipsPage.js
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import livingTips from "../../utils/livingTipsData";
 import './TipsPage.css';
 
 
 const TipsPage = () => {
-  const [tipsData, setTipsData] = useState([])
+  const [currentTip, setCurrentTip] = useState()
 
+  const getRandomTip = () => {
+    let randomTipIndex = Math.floor(Math.random() * livingTips.length);
+    let randomTip = livingTips[randomTipIndex].tip;
+    setCurrentTip(randomTip)
+  }
+
+  useEffect(() => {
+    getRandomTip()
+  }, []);
 
 
 
@@ -13,6 +22,8 @@ const TipsPage = () => {
   return (
     <section>
       <p>useful tip here</p>
+      {/* {console.log('currentTip: ', livingTips[currentTip])} */}
+      <p>{currentTip}</p>
     </section>
   )
 }
