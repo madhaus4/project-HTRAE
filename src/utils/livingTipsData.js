de99a19a414af36bc22da5c56df213219651ed27
Maria DelSignore <maria.ad27@gmail.com>
2021-09-25 15:13:16 -0600

    Add dataset into file

--- src/utils/livingTipsData.js
+++ src/utils/livingTipsData.js
@@ -1,6 +1,67 @@
 const livingTips = [
-
-
-]
+  {category: 'home', categoryData: [
+    'Completely turn off equipment like televisions and stereos when you\'re not using them.',
+    'Choose energy-efficient appliances and light bulbs.',
+    'Save water: some simple steps can go a long way in saving water like, like turning off the tap when you are brushing your teeth or shaving. Try to collect the water used to wash vegetables and salad to water your houseplants.',
+    'Lower your shades or close your curtains on hot days, to keep the house cool and reduce the use of electric fans or air-conditioning.',
+    'Let clothes dry naturally.',
+    'Keep lids on pans when cooking to conserve energy.',
+    'Use rechargeable batteries.',
+    'Call your local government to see if they have a disposal location for used batteries, glass, plastics, paper or other wastes.',
+    'Don’t use “throw away” products like paper plates and napkins, or plastic knives, forks and cups.',
+    'Send electronic greetings over email, instead of paper cards.'
+    ]
+  },
+  {category: 'garden', categoryData: [
+    'Collect rainwater to water your garden.',
+    'Water the garden early in the morning or late in the evening. This reduces water loss due to evaporation. Don’t over-water the garden. Water only until the soil becomes moist, not soggy.',
+    'Explore water efficient irrigation systems. Sprinkler irrigation and drip irrigation can be adapted to garden situations.',
+    'Make your garden lively - plant trees and shrubs that will attract birds. You can also put up bird nest boxes with food.',
+    'Put waste to work in your garden- sweep the fallen leaves and flowers into flowerbeds or under shrubs. This will increase soil fertility and also reduce the need for frequent watering.',
+    'If you have little space in your garden, you could make a compost pit to turn organic waste from the kitchen and garden to soil enriching manure.',
+    'Plant local species of trees, flowers and vegetables.',
+    'Don\'t use chemicals in the garden - as they will eventually end up in the water systems and can upset the delicate balance of lifecycles.',
+    'Organic and environmentally friendly fertilizers and pesticides are available - organic gardening reduces pollution and is better for wildlife.'
+    ]
+  },
+  {category: 'grocery shopping', categoryData: [
+    'Buy fruit and vegetables that are in season to help reduce enormous transport costs resulting from importing produce and, where possible, choose locally produced food.',
+    'When buying fish look out for a variety of non-endangered species and buy local fish if possible.',
+    'Bring your own bags to the grocery and refuse the plastic bags that create so much waste.',
+    'Look for products that have less packaging.'
+    ]
+  },
+  {category: 'work', categoryData: [
+    'Use printers that can print on both sides of the paper; try to look into this option when replacing old printers.',
+    'Use the back of a draft or unwanted printout instead of notebooks. Even with a double-sided printer there is likely to be plenty of spare paper to use!',
+    'Always ask for and buy recycled paper if you can - for your business stationery and to use in your printers.',
+    'Switch off computer monitors, printers and other equipment at the end of each day. Though in standby mode they\'re still using power - and that adds to global warming. Always turn off your office light and computer monitor when you go out for lunch or to a meeting.'
+    ]
+  },
+  {category: 'vacation', categoryData: [
+    'Go on holiday during the off-peak period to prevent over straining resources - you\'ll also avoid the crowds.',
+    'Find out about places before you visit. You may be visiting an environmentally sensitive area, in which case you must take extra care to stay on footpaths and follow signs.',
+    'Don\'t travel by air if you can avoid it because air travel uses up large amounts of fossil fuels and creates greenhouse gases.',
+    'Dispose of any rubbish responsibly - it can be hazardous to wildlife.',
+    'Use public transport, cycle or walk instead of using a car.',
+    'Use facilities and trips run by local people whenever possible.',
+    'Don\'t be tempted to touch wildlife and disturb habitats whether on land, at the coast or under water.',
+    'Be careful what you choose to bring home as a holiday souvenir. Many species from coral and conch shells to elephants and alligators are endangered because they are killed for curios or souvenirs.',
+    'Don’t leave any rubbish at the beach – it can be very dangerous for the wildlife.',
+    'Boats and jet-skis create noise and chemical pollution that is very disturbing to wildlife - don\'t keep the engine running unnecessarily.'
+    ]
+  },
+  {category: 'reduce, reuse, recycle', categoryData: [
+    'Always use both sides of a sheet of paper.',
+    'Use e-mail to stay in touch, including cards, rather than faxing or writing.',
+    'Share magazines with friends and pass them on to the doctor, dentist or local hospital for their waiting rooms.',
+    'Use recyclable paper to make invitation cards, envelops, letter pads etc., if you can.',
+    'Use washable nappies instead of disposables if you can.',
+    'Recycle as much as you can.',
+    'Give unwanted clothes, toys and books to charities and orphanages.',
+    'Store food and other products in containers rather than foil and plastic wrap.'
+    ]
+  }
+];
 
 export default livingTips;
