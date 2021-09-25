7b118b1f46e575cb5e6560da9904d749f8c081b7
Maria DelSignore <maria.ad27@gmail.com>
2021-09-25 14:49:05 -0600

    Create tips files

---
+++ src/components/TipsPage/TipsPage.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import livingTips from "../../utils/livingTipsData";
+import './TipsPage.css';
+
+
+const TipsPage = () => {
+  const [tipsData, setTipsData] = useState([])
+
+  return (
+    <section>
+      <p>useful tip here</p>
+    </section>
+  )
+}
+
+
+export default TipsPage;
