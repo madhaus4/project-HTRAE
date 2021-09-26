06785d1883f83478535c42795d518f2488308df1
Maria DelSignore <maria.ad27@gmail.com>
2021-09-26 14:04:20 -0600

    Add parenths

--- src/components/TipsPage/DisplayTips.js
+++ src/components/TipsPage/DisplayTips.js
@@ -2,7 +2,9 @@ import './TipsPage.css';
 
 const DisplayTips = ({ favoriteTips }) => {
   const getFavorites = favoriteTips.map(favorite => {
-    return <p key={Date.now()}>{favorite.tip}</p>
+    return (
+      <p key={Date.now()}>{favorite.tip}</p>
+    )
   })
 
   return (
