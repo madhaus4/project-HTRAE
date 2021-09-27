56699884ed2359e8933c55d1612aafa811ef282d
Maria DelSignore <maria.ad27@gmail.com>
2021-09-26 18:47:22 -0600

    Add refrence links to homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -8,12 +8,12 @@ const HomePage = () => {
       <h3>Welcome to project HTRAE</h3>
       <p>Your quick resource on global warming effects.</p>
       <h4>What is climate change?</h4>
-      <p></p>
+      <p>The short is that climate change is the long-term shifts in temperatures and weather patterns.  Mostly caused by human activities such as burning fossil fuels such as natural gases, oils and coal.  Burning these fossil fuels causes greenhouse as emissions that wrap around Earth and trap the sun's heat and rises temperatures.</p>
     </div>
     <div>
       <p>For more information: </p>
-      <a href=''></a>
-      <a href=''></a>
+      <a href='https://www.un.org/en/climatechange'>UN on climate change</a>   
+      <a href='https://explore.panda.org/climate'>WWF on climate crisis</a>
     </div>
     </section>
   )
