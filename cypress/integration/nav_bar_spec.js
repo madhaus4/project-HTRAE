541effcab1c65d53334e62b4783b91b56ad41c73
Maria DelSignore <maria.ad27@gmail.com>
2021-09-27 14:40:38 -0600

    Add button testing

---
+++ cypress/integration/nav_bar_spec.js
@@ -0,0 +1,60 @@
+describe('Nav Bar', () => {
+  beforeEach (() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Should have a button for each page', () => {
+    cy.get('nav')
+      .get('.btn-container')
+      .contains('Home')
+      .click()
+      .url()
+      .should('include', '/')
+  })
+
+  it('Should have a button for each page', () => {
+    cy.get('nav')
+      .get('.btn-container')
+      .contains('CO2')
+      .click()
+      .url()
+      .should('include', '/Carbon-Dioxide-Levels')
+  })
+
+  it('Should have a button for each page', () => {
+    cy.get('nav')
+      .get('.btn-container')
+      .contains('Surface Temp')
+      .click()
+      .url()
+      .should('include', '/Temperature')
+  })
+
+  it('Should have a button for each page', () => {
+    cy.get('nav')
+      .get('.btn-container')
+      .contains('Glacial Melt')
+      .click()
+      .url()
+      .should('include', '/Arctic-Ice-Melt')
+  })
+
+  it('Should have a button for each page', () => {
+    cy.get('nav')
+      .get('.btn-container')
+      .contains('Sustainable Tips')
+      .click()
+      .url()
+      .should('include', '/Sustainable-Life-Tips')
+  })
+
+  it('Should have a button for each page', () => {
+    cy.get('nav')
+      .get('.btn-container')
+      .contains('This Planet')
+      .click()
+      .url()
+      .should('include', '/Reminder')
+  })
+
+})
