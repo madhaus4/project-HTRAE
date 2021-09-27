fecfb3276ef664cfa92410c0266583ed3585c981
Maria DelSignore <maria.ad27@gmail.com>
2021-09-27 16:27:59 -0600

    Test sad path for img and btn

--- cypress/integration/temp_page_spec.js
+++ cypress/integration/temp_page_spec.js
@@ -16,7 +16,13 @@ describe('Home Page', () => {
   it('Should display a graph', () => {
     cy.get('.temp-page-container')
       .get('.temp-data-graph')
-      
   })
 
+  it('Should not display an image', () => {
+    cy.contains('img').should('not.exist')
+  })
+
+  it('Should not display a button', () => {
+    cy.contains('button').should('not.exist')
+  })
 })
