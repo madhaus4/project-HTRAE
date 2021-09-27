f0c531053b41fec2bc9a9a190862f883ed9c4ea6
Maria DelSignore <maria.ad27@gmail.com>
2021-09-27 17:11:09 -0600

    Remove console log

--- src/components/PolarPage/PolarGraph.js
+++ src/components/PolarPage/PolarGraph.js
@@ -1,22 +1,9 @@
 // import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
-import {XYPlot, XAxis, YAxis, LineMarkSeries, VerticalGridLines, HorizontalGridLines, VerticalBarSeries, MarkSeries, LineSeries, CircularGridLines} from 'react-vis';
+import {XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, LineSeries} from 'react-vis';
 import '../../../node_modules/react-vis/dist/style.css';
 import './PolarPage.css';
 
 const PolarGraph = ({ polarData }) => {
-
-  
-  // const margin = {
-  //   top: 10,
-  //   bottom: 10,
-  //   left: 10,
-  //   right: 10
-  // };
-  
-  // const WIDTH = 300;
-  // const HEIGHT = 300;
-
-
 
   return (  
     <XYPlot
@@ -42,26 +29,3 @@ const PolarGraph = ({ polarData }) => {
 
 
 export default PolarGraph;
-
-
-{/* <XYPlot
-margin={margin}
-xDomain={[-3, 3]}
-yDomain={[-3, 3]}
-width={WIDTH}
-height={HEIGHT}
->
-<CircularGridLines />
-<XAxis top={(HEIGHT - margin.top) / 2} />
-<YAxis left={(WIDTH - margin.left - margin.right) / 2} />
-<LineSeries
-  color="#B8466F"
-  strokeWidth={2}
-  sizeRange={[5, 15]}
-  data={polarData.map(elem => ({
-    ...elem,
-    x: Math.cos(elem.extent) * elem.extent,
-    y: Math.sin(elem.area) * elem.area
-  }))}
-/>
-</XYPlot> */}
