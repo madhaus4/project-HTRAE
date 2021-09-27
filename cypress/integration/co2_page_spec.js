bb5c54831429b6995469942627800b6ba52a6b97
Maria DelSignore <maria.ad27@gmail.com>
2021-09-27 15:10:21 -0600

    Add tests for default text

---
+++ cypress/integration/co2_page_spec.js
@@ -0,0 +1,22 @@
+describe('Home Page', () => {
+  beforeEach (() => {
+    // cy.intercept('GET', 'https://global-warming.org/api/co2-api', { fixture: 'carbon_dioxide'}).as('CO2Data')
+    cy.visit('http://localhost:3000/Carbon-Dioxide-Levels')
+  })
+
+  it('Should display text content by default', () => {
+    cy.get('.CO2-page-container')
+      .get('h2')
+      .should('include.text', 'Carbon dioxide (CO2) is a greenhouse gas')
+    cy.get('.CO2-page-container')
+      .get('p')
+      .should('include.text', '~26 parts per million')
+  })
+
+  it('Should display a graph', () => {
+    cy.get('.CO2-page-container')
+      .get('.CO2-data-graph')
+      
+  })
+
+})
