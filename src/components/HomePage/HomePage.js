1b689c4c3cc0e3acebbdf4a72b51d4bccaacbfd4
Maria DelSignore <maria.ad27@gmail.com>
2021-09-26 18:39:49 -0600

    Add home page content

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,10 +2,20 @@ import './HomePage.css';
 
 const HomePage = () => {
   return (
+    <section className='home-page-container'>
     <div>
-      <h2>Welcome to project HTRAE</h2>
-      <p>resource on global warming effects.  out here trying to reverse the direction our planet's environment is heading</p>
+      <h2>Worldwide, the climate crisis is felt.  Everything in our environment is changing and this change impacts everything around us.  If the global temperature continues to rise, effects will worsen.  We need all of humanity's help to reverse what we've already done as a society.</h2>
+      <h3>Welcome to project HTRAE</h3>
+      <p>Your quick resource on global warming effects.</p>
+      <h4>What is climate change?</h4>
+      <p></p>
     </div>
+    <div>
+      <p>For more information: </p>
+      <a href=''></a>
+      <a href=''></a>
+    </div>
+    </section>
   )
 }
 
