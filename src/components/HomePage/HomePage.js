1216fe37eb2cbecab3475bd31ca07e8198642c26
Maria DelSignore <maria.ad27@gmail.com>
2021-09-27 09:46:02 -0600

    Update verbage in content

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <section className='home-page-container'>
     <div className='home-page-intro'>
-      <h2>Worldwide, the climate crisis is felt.  Everything in our environment is changing and this change impacts everything around us.  If the global temperature continues to rise, effects will worsen.  We need all of humanity's help to reverse what we've already done as a society.</h2>
+      <h2>Worldwide, the climate crisis is felt.  Everything in our environment is changing and this change sparks a chain reaction that impacts all aspects of life around us.  If the global temperature continues to rise, effects will worsen.  We need all of humanity's help to reverse what we've already done as a society.</h2>
       <span className='welcome-statement'><h3>Welcome to project HTRAE</h3><p>Your quick resource on global warming effects.</p></span>
       <span className='climate-statement'><h4>What is climate change?</h4>
       <p>The short is that climate change is the long-term shifts in temperatures and weather patterns.  Mostly caused by human activities such as burning fossil fuels such as natural gases, oils and coal.  Burning these fossil fuels causes greenhouse as emissions that wrap around Earth and trap the sun's heat and rises temperatures.</p></span>
