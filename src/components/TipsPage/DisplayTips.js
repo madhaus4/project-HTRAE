800b663ee9d6f039c4a3a604d8a3f24a48b5bf2b
Maria DelSignore <maria.ad27@gmail.com>
2021-09-27 11:50:22 -0600

    pass update favorite method to each favorite

--- src/components/TipsPage/DisplayTips.js
+++ src/components/TipsPage/DisplayTips.js
@@ -1,15 +1,21 @@
 import arrowFill from '../../arrow-fill.png';
+import saved from '../../bookmark-fill.png';
+import unsaved from '../../bookmark-empty.png';
 import './TipsPage.css';
 
-const DisplayTips = ({ favoriteTips }) => {
+const DisplayTips = ({ favoriteTips, updateFavorite }) => {
   const getFavorites = favoriteTips.map(favorite => {
     return (
       <article>
         <div className='saved-tips'>
-          <img className='arrow-icon' src={arrowFill} />
-          <p key={Date.now()}>{favorite.tip}</p>
+          <img 
+            onClick={() => updateFavorite(favorite)}
+            className='arrow-icon' 
+            src={saved} 
+          />
+          <p key={favorite.id}>{favorite.tip}</p>
         </div>
-        <h4>X</h4>
+        {/* <h4>X</h4> */}
       </article>
     )
   })
