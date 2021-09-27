804ef1fa94332d79649a4aa6884e3c1ca1cc0a7c
Maria DelSignore <maria.ad27@gmail.com>
2021-09-26 20:33:30 -0600

    Add image data set

---
+++ src/utils/beautifulWorldData.js
@@ -0,0 +1,74 @@
+const otherWorldly = [
+  {
+    img: 'https://www.edf.org/sites/default/files/facebook_thumb/GettyImages-155141288-polar-bear-1200x630-2.jpg',
+    description: '',
+    date: '',
+    copyright: 'Josef Friedhuber'
+  },
+  {
+    img: 'https://themysteriousworld.com/wp-content/uploads/2013/11/aurora-borealis.jpg',
+    description: 'Aurora Borealis or northern lights is a naturally occurring spectacular display of lights in the sky. It can be seen in Northern and Southern hemisphere.',
+    date: '',
+    copyright: ''
+  },  
+  {
+    img: 'https://themysteriousworld.com/wp-content/uploads/2013/11/victoria-falls.webp',
+    description: 'Victoria waterfalls is the largest waterfalls in the world depending on height and width. It is located in the border of Zambia and Zimbabwe, also known as mosi-oa-tanya.. The waterfall is protected by mosi-oa-tanya and Victoria national parks.',
+    date: '',
+    copyright: ''
+  },
+  {
+    img: 'https://static.onecms.io/wp-content/uploads/sites/28/2016/12/201303-w-strangest-natural-wonders-marble-caves-chile.jpg',
+    description: 'Marble Caves, Chile',
+    date: '',
+    copyright: 'Robert O\'Duill'
+  },
+  {
+    img: 'https://themysteriousworld.com/wp-content/uploads/2013/11/paricutin-volcano.webp',
+    description: 'Particutin volcano is a cinder cone volcano situated in Mexico. The mountain standing at a height of 2800 m. The area is highly prone to earthquakes and volcanic eruptions.',
+    date: '',
+    copyright: ''
+  },
+  {
+    img: 'https://themysteriousworld.com/wp-content/uploads/2013/11/grand-canyon.webp',
+    description: 'Grand Canyon is the steep sided massive canyon located in Arizona, US. Grand Canyon is formed by the continuous action of Colorado river, it is one of the globally popular national parks in Unites States.',
+    date: '',
+    copyright: ''
+  },
+  {
+    img: 'https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/10/12/new-seven-wonders-machu-picchu.jpg.rend.hgtvcom.1280.960.suffix/1491581548990.jpeg',
+    description: 'Machu Picchu, an Incan city of sparkling granite precariously perched between 2 towering Andean peaks, is thought by scholars to have been a sacred archaeological center for the nearby Incan capital of Cusco.',
+    date: '',
+    copyright: ''
+  },
+  {
+    img: 'https://themysteriousworld.com/wp-content/uploads/2013/11/great-barrier-reef.webp',
+    description: 'Great barrier reef is the largest under water structure  in the world. It contain more than 3000 individual reef systems. Great barrier reef is one of world heritage listed places and a greatest natural wonder on Earth.',
+    date: '',
+    copyright: 'Bruce Tuten'
+  },
+  {
+    img: 'https://scontent.fapa1-1.fna.fbcdn.net/v/t1.6435-9/81375682_174658483927885_2406635390455775232_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=gLncvCyW5D0AX_uu_e7&_nc_oc=AQl16WjXNp-mpq3Tug964dffM7mIPBliBUjGXFNRursQy_WqQcXVSiH5rl1OqIYFwIZemMu-0Zhf4B0_yAoYmxeb&_nc_ht=scontent.fapa1-1.fna&oh=81730618566cd12be498de4a4abc4601&oe=61760135',
+    description: '',
+    date: '',
+    copyright: ''
+  },
+  {
+    img: 'https://themysteriousworld.com/wp-content/uploads/2013/11/death-valley.webp',
+    description: 'Death valley is one of the most popular national parks of United States located in Eastern California. It covers a vast area of 3.1 million acres. Death valley is also called as funeral mountains, hell’s gate and starvation canyon.',
+    date: '',
+    copyright: ''
+  },
+  {
+    img: 'https://scontent.fapa1-1.fna.fbcdn.net/v/t1.6435-9/70625335_105971367463264_8872828162048786432_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=EmiRlZ5-NyEAX8YRXBB&tn=nt1RDwyC8c5SK0U3&_nc_ht=scontent.fapa1-1.fna&oh=da51ba7edcc444e1c205aadcab935829&oe=6178B57B',
+    description: '',
+    date: '',
+    copyright: ''
+  },
+  {
+    img: 'https://themysteriousworld.com/wp-content/uploads/2013/11/mount-everest.webp',
+    description: 'Mount Everest is the highest spot on Earth with height of 8848 m, located between the borders of Tibet and Nepal in Himalayan mountains. It is named after George Everest , surveyor general of Britain.',
+    date: '',
+    copyright: ''
+  }
+]
