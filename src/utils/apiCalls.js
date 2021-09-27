6a326b2821c7cd35de534850e2d4217979913aa4
Maria DelSignore <maria.ad27@gmail.com>
2021-09-27 17:03:17 -0600

    Remove unused fetch call

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,16 +1,8 @@
-// export const getData = async () => {
-//   return fetch(`https://global-warming.org/api/temperature-api`)
-//     .then(res => res.json())
-//     .then(data => console.log(data))
-// }
-
-
 export const getData = async (category) => {
   let response = await fetch(`https://global-warming.org/api/${category}-api`)
 
   if (!response.ok) {
     throw new Error('this no good')
   }
-
   return await response.json();
 }
