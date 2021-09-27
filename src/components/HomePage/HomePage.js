c2abce1aaef661f2b2dae4b1e5ad94b64952ecdd
Maria DelSignore <maria.ad27@gmail.com>
2021-09-27 09:42:10 -0600

    Add class names to content

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,14 +3,13 @@ import './HomePage.css';
 const HomePage = () => {
   return (
     <section className='home-page-container'>
-    <div>
+    <div className='home-page-intro'>
       <h2>Worldwide, the climate crisis is felt.  Everything in our environment is changing and this change impacts everything around us.  If the global temperature continues to rise, effects will worsen.  We need all of humanity's help to reverse what we've already done as a society.</h2>
-      <h3>Welcome to project HTRAE</h3>
-      <p>Your quick resource on global warming effects.</p>
-      <h4>What is climate change?</h4>
-      <p>The short is that climate change is the long-term shifts in temperatures and weather patterns.  Mostly caused by human activities such as burning fossil fuels such as natural gases, oils and coal.  Burning these fossil fuels causes greenhouse as emissions that wrap around Earth and trap the sun's heat and rises temperatures.</p>
+      <span className='welcome-statement'><h3>Welcome to project HTRAE</h3><p>Your quick resource on global warming effects.</p></span>
+      <span className='climate-statement'><h4>What is climate change?</h4>
+      <p>The short is that climate change is the long-term shifts in temperatures and weather patterns.  Mostly caused by human activities such as burning fossil fuels such as natural gases, oils and coal.  Burning these fossil fuels causes greenhouse as emissions that wrap around Earth and trap the sun's heat and rises temperatures.</p></span>
     </div>
-    <div>
+    <div className='resource-container'>
       <p>For more information: </p>
       <a href='https://www.un.org/en/climatechange'>UN on climate change</a>   
       <a href='https://explore.panda.org/climate'>WWF on climate crisis</a>
